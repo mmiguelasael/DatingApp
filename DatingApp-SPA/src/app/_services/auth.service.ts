98f34ade52767c1ad32e8becfe991991513753a2
Miguel Asael Martha Lopez <miguel.aml92@hotmail.com>
2019-02-14 19:07:11 -0700

    End of Section 6

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '../../../node_modules/@angular/common/http';
 import { map } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { tokenKey } from '../../../node_modules/@angular/core/src/view';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   baseurl = 'http://localhost:5000/api/auth/';
+  jwtHelper = new JwtHelperService();
+  decodedToken: any;
 
 constructor(private http: HttpClient) { }
 
@@ -16,6 +20,8 @@ constructor(private http: HttpClient) { }
           const user = response;
           if (user) {
             localStorage.setItem('token', user.token);
+            this.decodedToken = this.jwtHelper.decodeToken(user.token);
+            console.log(this.decodedToken);
           }
         })
       );
@@ -23,5 +29,10 @@ constructor(private http: HttpClient) { }
 
   register(model: any) {
     return this.http.post(this.baseurl + 'register', model);
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !this.jwtHelper.isTokenExpired(token);
   }
 }
